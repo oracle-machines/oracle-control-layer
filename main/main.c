5c798da1942a7a7ddfb9040cd0411b815096dc5d
Hassan Kamran <hassan-kamran@github.com>
2022-12-24 20:45:15 +0000

    add pwm generator

--- main/main.c
+++ main/main.c
@@ -1,7 +1,30 @@
-#include <stdio.h>
-#include "sdkconfig.h"
+#include "driver/mcpwm.h"
 
-void app_main(void)
+// Set the GPIO pin numbers for the H-bridge inputs
+#define PIN_PWM_IN_A 18
+#define PIN_PWM_IN_B 19
+
+// Set the frequency and duty cycle for the PWM signal
+#define PWM_FREQUENCY 1000
+#define PWM_DUTY_CYCLE 50
+
+void app_main()
 {
-    printf("Hello world!\n");
+    // Initialize the MCPWM module
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0A, PIN_PWM_IN_A);
+    mcpwm_gpio_init(MCPWM_UNIT_0, MCPWM0B, PIN_PWM_IN_B);
+    mcpwm_config_t pwm_config;
+    pwm_config.frequency = PWM_FREQUENCY;
+    pwm_config.cmpr_a = 0;
+    pwm_config.cmpr_b = 0;
+    pwm_config.counter_mode = MCPWM_UP_COUNTER;
+    pwm_config.duty_mode = MCPWM_DUTY_MODE_0;
+    mcpwm_init(MCPWM_UNIT_0, MCPWM_TIMER_0, &pwm_config);
+
+    // Set the duty cycle for each input of the H-bridge
+    mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_A, PWM_DUTY_CYCLE);
+    mcpwm_set_duty(MCPWM_UNIT_0, MCPWM_TIMER_0, MCPWM_OPR_B, PWM_DUTY_CYCLE);
+
+    // Start the PWM signal
+    mcpwm_start(MCPWM_UNIT_0, MCPWM_TIMER_0);
 }
